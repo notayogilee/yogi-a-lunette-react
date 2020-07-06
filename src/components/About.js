f662d7ae7e5f92c722264b0709ece8e1b0abad29
Lee Castelani <notayogilee@gmail.com>
2020-07-06 17:54:05 -0400

    add real about text

--- src/components/About.js
+++ src/components/About.js
@@ -8,7 +8,12 @@ const About = () => {
       <img src={yogiAbout} className="yogi-pic" alt="none" />
       <div className="text">
         <h2>About Me</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Expedita aliquid nulla debitis commodi unde pariatur repellendus ipsam cum quas, possimus consequatur laudantium est suscipit consequuntur mollitia non deleniti hic corporis qui magnam? Praesentium repellat iste adipisci porro tempora magnam fugit illum tempore magni aliquam et, atque reprehenderit? Voluptate libero vel soluta numquam ipsa ducimus dolorum, quasi commodi, repudiandae aperiam, veritatis delectus unde quod eveniet blanditiis iusto et adipisci tempore rerum sit magni enim est. Reiciendis id nemo blanditiis deleniti expedita soluta! Placeat dolore id et expedita! Vitae sed soluta possimus nihil veritatis dicta sapiente, ducimus blanditiis quisquam alias porro odit.</p>
+        <p>Claudia Viens pratique Asana-Yoga depuis 2006 et enseigne depuis 2017. Reconnue pour sa passion, sa clarté et sa créativité, son style d'enseignement est un mélange dynamique et stimulant d'inspiration, d'humour et de dur labeur. Maîtrise de la synthèse, sa capacité à récolter et à transmettre les contributions uniques de diverses méthodes de yoga est sans précédent. Claudia croit que la pratique du yoga peut aider n'importe qui à accéder à sa sagesse intérieure, à sa spiritualité authentique et à clarifier sa plus haute expression personnelle.
+ 
+ Claudia s'inspire de nombreux maîtres enseignants descendants du Hatha Yoga, comme: Iyengar, Anusara, Ashtanga, Bikram et de la technologie Kundalini. Elle est professeure certifiée en Hatha-Vinyasa du Collège Sangha Yoga. Elle est aussi vloggeuse sur sa chaîne Youtube "Yogi-à-lunette". Créatrice de classes qui sortent de l'ordinaire, de tutoriels regorgeant de détails et d'ateliers surprenants dans l'intégration de  différentes pratiques,  Claudia vous fera vivre une expérience hors du commun à chaque rencontre. Depuis 2019,  elle s'est joint à la grande famille IDOLEM qui lui a ouvert sa porte et son cœur, permettant de transmettre, à cette communauté désireuse de co-créer, son amour du Yoga.  
+  
+ Claudia  réside dans le quartier St-Henri de Montréal avec son mari, sa sœur et  deux toutous gentils comme tout, Loky & Jasper. Ces grandes passions sont le yoga, la lecture, la cuisine et les aventures dans les friperies et les ventes de garage ! :)
+ </p>
       </div>
     </div>
   )
