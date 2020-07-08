f379f6e4bac08e1b7dce8b2adef977eda14a1799
Lee Castelani <notayogilee@gmail.com>
2020-07-08 16:13:23 -0400

    fix some errors with table layout

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -22,25 +22,25 @@ const Contact = () => {
               </thead>
               <tbody>
                 <tr>
-                  <a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer"><th>Idolem Brossard <i className="fas fa-map-marker-alt"></i></th></a>
+                  <th><a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer">Idolem Brossard <i className="fas fa-map-marker-alt"></i></a></th>
                   <th>Intermediaire/Avanacé: Power Flow</th>
                   <th>Mardi</th>
                   <th>18:30-19:30</th>
                 </tr>
                 <tr>
-                  <a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer"><th>Idolem Brossard <i className="fas fa-map-marker-alt"></i></th></a>
+                  <th><a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer">Idolem Brossard <i className="fas fa-map-marker-alt"></i></a></th>
                   <th>Tous Niveaux: Yin Nidra</th>
                   <th>Mardi</th>
                   <th>20:00-21:00</th>
                 </tr>
                 <tr>
-                  <a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer"><th>Idolem Brossard <i className="fas fa-map-marker-alt"></i></th></a>
+                  <th><a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer">Idolem Brossard <i className="fas fa-map-marker-alt"></i></a></th>
                   <th>Tous Niveaux: Yoga Astanga Fusion</th>
                   <th>Samedi</th>
                   <th>9:00-10:00</th>
                 </tr>
                 <tr>
-                  <a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer"><th>Idolem Brossard <i className="fas fa-map-marker-alt"></i></th></a>
+                  <th><a href="https://www.google.com/maps/place/5840+Taschereau+Blvd,+Brossard,+QC+J4W+1M6/@45.4768478,-73.4692905,17z/data=!3m1!4b1!4m5!3m4!1s0x4cc90573f84838c3:0xe0e26f62c9164785!8m2!3d45.4768478!4d-73.4671018" target="_blank" rel="noopener noreferrer">Idolem Brossard <i className="fas fa-map-marker-alt"></i></a></th>
                   <th>Débutant: Flow Énergétique</th>
                   <th>Samedi</th>
                   <th>10:00-11:00</th>
