4d5bcd6b28d68186ee55eccea9c9ba2526685cde
Lee Castelani <notayogilee@gmail.com>
2020-07-17 17:05:45 -0400

    fix text on about

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.scss';
 
 const Contact = () => {
+
+  const [slide, setSlide] = useState('');
+
   return (
     <div id="contact">
       <div className="contact-container">
         <div className="bubbles">
-          <a href="#"><div className="bubble-1"><i class="far fa-calendar-alt fa-4x"></i></div></a>
-          <a href="#"><div className="bubble-2"><i class="far fa-comments fa-4x"></i></div></a>
-          <a href="#"><div className="bubble-3"><i class="far fa-envelope fa-4x"></i></div></a>
+          <a href="#" className={slide}><div className="bubble-1"><i class="far fa-calendar-alt fa-4x"></i></div></a>
+          <div className="schedule"></div>
+          <a href="#" className={slide}><div className="bubble-2"><i class="far fa-comments fa-4x"></i></div></a>
+          <div className="schedule"></div>
+          <a href="#" className={slide}><div className="bubble-3"><i class="far fa-envelope fa-4x"></i></div></a>
+          <div className="schedule"></div>
         </div>
       </div>
     </div>
