fdc5804285699f8bc0a56bceacde320c0424466c
Lee Castelani <notayogilee@gmail.com>
2020-07-22 18:47:21 -0400

    fix sidebar and display of contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,19 +6,39 @@ const Contact = () => {
   const [slide, setSlide] = useState('');
 
   return (
-    <div id="contact">
+    <div id="contact-page">
       <div className="contact-container">
         <div className="bubbles">
-          <a href="#" className={slide}><div className="bubble-1"><i class="far fa-calendar-alt fa-4x"></i></div></a>
-          <div className="schedule"></div>
-          <a href="#" className={slide}><div className="bubble-2"><i class="far fa-comments fa-4x"></i></div></a>
-          <div className="schedule"></div>
-          <a href="#" className={slide}><div className="bubble-3"><i class="far fa-envelope fa-4x"></i></div></a>
-          <div className="schedule"></div>
+          <a href="#" className={slide}>
+            <div className="bubble-1">
+              <i class="far fa-calendar-alt fa-4x"></i>
+              <h1>Schedule</h1>
+            </div>
+          </a>
+          <a href="#">
+            <div className="bubble-2">
+              <i class="far fa-comments fa-4x"></i>
+              <h1>contact</h1>
+              <div className="contact">
+                <h2>claudiaviens@gmail.com</h2>
+                <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-facebook-square fa-2x"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer">
+                  <i className="fab fa-linkedin fa-2x"></i>
+                </a>
+              </div>
+            </div>
+          </a>
+          <a href="#" className={slide}>
+            <div className="bubble-3">
+              <i class="far fa-envelope fa-4x"></i>
+              <h1>message</h1>
+            </div>
+          </a>
         </div>
       </div>
     </div>
-
   )
 }
 
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,58 +4,53 @@ import './Sidebar.scss';
 
 const Sidebar = () => {
 
-  const [open, setOpen] = useState(false);
   const [hidden, setHidden] = useState("");
 
   return (
     <div className="big-container">
-      {!open &&
-        <div className="hamburger">
-          <a onClick={() => { setOpen(true); setHidden("hidden") }} href="#">
-            <div className="bar-1"></div>
-            <div className="bar-2"></div>
-            <div className="bar-3"></div>
-          </a>
-        </div>
-      }
+      <div className="hamburger">
+        <a onClick={() => { setHidden("hidden") }} href="#">
+          <div className="bar-1"></div>
+          <div className="bar-2"></div>
+          <div className="bar-3"></div>
+        </a>
+      </div>
       <div id="sidebar" className={hidden}>
         <div className="sidebar-container">
-          {open &&
-            <Fragment>
-              <div className="x-hamburger">
-                <a onClick={() => { setOpen(false); setHidden("") }} href="#">
-                  <div className="bar-4"></div>
-                  <div className="bar-5"></div>
-                  <div className="bar-6"></div>
-                </a>
+          <Fragment>
+            <div className="x-hamburger">
+              <a onClick={() => { setHidden("") }} href="#">
+                <div className="bar-4"></div>
+                <div className="bar-5"></div>
+                <div className="bar-6"></div>
+              </a>
+            </div>
+            <div className="links">
+              <div className="side-list">
+                <ul>
+                  <li><Link onClick={() => { setHidden("") }} to="/about" >About</Link></li>
+                  <li><Link onClick={() => { setHidden("") }} to="/videos" >Videos</Link></li>
+                  <li><Link onClick={() => { setHidden("") }} to="/contact" >Contact</Link></li>
+                </ul>
               </div>
-              <div className="links">
-                <div className="side-list">
-                  <ul>
-                    <li><Link to="/about" >About</Link></li>
-                    <li><Link to="/videos" >Videos</Link></li>
-                    <li><Link to="/contact" >Contact</Link></li>
-                  </ul>
-                </div>
-                <div className="social">
-                  <ul>
-                    <li>
-                      <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                      <a href="https://www.youtube.com/channel/UC9u2sGj3VZpR0KAGCF_BvUw" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                      <a href="https://www.instagram.com/yogialunette/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square fa-2x"></i></a>
-                    </li>
-                    <li>
-                      <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
-                    </li>
-                  </ul>
-                </div>
+              <div className="social">
+                <ul>
+                  <li>
+                    <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square fa-2x"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.youtube.com/channel/UC9u2sGj3VZpR0KAGCF_BvUw" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube-square fa-2x"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.instagram.com/yogialunette/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square fa-2x"></i></a>
+                  </li>
+                  <li>
+                    <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+                  </li>
+                </ul>
               </div>
-            </Fragment>
-          }
+            </div>
+          </Fragment>
         </div>
       </div>
     </div>
