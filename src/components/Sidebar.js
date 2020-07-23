c8e35b326f3d53be5b4692501c52c179448003e6
Lee Castelani <notayogilee@gmail.com>
2020-07-23 17:32:00 -0400

    change hamburger menu

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,22 +4,53 @@ import './Sidebar.scss';
 
 const Sidebar = () => {
 
-  const [hidden, setHidden] = useState("");
+  const [show, setShow] = useState("");
+  // const [toggle, setToggle] = useState("");
 
   return (
-    <div className="big-container">
-      <div className="hamburger">
-        <a onClick={() => { setHidden("hidden") }} href="#">
-          <div className="bar-1"></div>
-          <div className="bar-2"></div>
-          <div className="bar-3"></div>
-        </a>
+    <Fragment>
+      <div className="menu-wrap">
+        <input type="checkbox" className="toggler" onClick={() => setShow("show")} />
+        <div className="hamburger"><div></div></div>
+
+        {/* <div className="menu">
+        <div>
+          <div>
+            <ul>
+              <li><Link onClick={() => { setShow("") }} to="/about" >About</Link></li>
+              <li><Link onClick={() => { setShow("") }} to="/videos" >Videos</Link></li>
+              <li><Link onClick={() => { setShow("") }} to="/contact" >Contact</Link></li>
+              <li>
+                <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square fa-2x"></i></a>
+              </li>
+              <li>
+                <a href="https://www.youtube.com/channel/UC9u2sGj3VZpR0KAGCF_BvUw" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube-square fa-2x"></i></a>
+              </li>
+              <li>
+                <a href="https://www.instagram.com/yogialunette/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square fa-2x"></i></a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div> */}
       </div>
-      <div id="sidebar" className={hidden}>
-        <div className="sidebar-container">
-          <Fragment>
+
+      <div className="big-container">
+        {/* <div className="hamburger">
+          <a onClick={() => { setShow("show") }} href="#">
+            <div className="bar-1"></div>
+            <div className="bar-2"></div>
+            <div className="bar-3"></div>
+          </a>
+        </div> */}
+        <div id="sidebar" className={show}>
+          <div className="sidebar-container">
+
             <div className="x-hamburger">
-              <a onClick={() => { setHidden("") }} href="#">
+              <a onClick={() => { setShow("") }} href="#">
                 <div className="bar-4"></div>
                 <div className="bar-5"></div>
                 <div className="bar-6"></div>
@@ -28,9 +59,9 @@ const Sidebar = () => {
             <div className="links">
               <div className="side-list">
                 <ul>
-                  <li><Link onClick={() => { setHidden("") }} to="/about" >About</Link></li>
-                  <li><Link onClick={() => { setHidden("") }} to="/videos" >Videos</Link></li>
-                  <li><Link onClick={() => { setHidden("") }} to="/contact" >Contact</Link></li>
+                  <li><Link onClick={() => { setShow("") }} to="/about" >About</Link></li>
+                  <li><Link onClick={() => { setShow("") }} to="/videos" >Videos</Link></li>
+                  <li><Link onClick={() => { setShow("") }} to="/contact" >Contact</Link></li>
                 </ul>
               </div>
               <div className="social">
@@ -50,10 +81,10 @@ const Sidebar = () => {
                 </ul>
               </div>
             </div>
-          </Fragment>
+          </div>
         </div>
       </div>
-    </div>
+    </Fragment>
   )
 }
 
