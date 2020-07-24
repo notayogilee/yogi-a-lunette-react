be61d8f391d7f8f2febf0cc27345ea15ab874fc5
Lee Castelani <notayogilee@gmail.com>
2020-07-24 10:22:28 -0400

    sidebar hamburger menu functional

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,46 +4,53 @@ import './Sidebar.scss';
 
 const Sidebar = () => {
 
-  const [show, setShow] = useState("");
+  const [show, setShow] = useState(false);
   // const [toggle, setToggle] = useState("");
+
 
   return (
     <Fragment>
       <div className="menu-wrap">
-        <input type="checkbox" className="toggler" onClick={() => setShow("show")} />
-        <div className="hamburger"><div></div></div>
-      </div>
+        <input type="checkbox" id="togglerCheckbox" className="toggler" onClick={() => setShow(!show)} />
 
-      <div id="sidebar" className={show}>
-        <div className="links">
-          <div className="side-list">
-            <ul>
-              <li><Link onClick={() => { setShow("") }} to="/about" >About</Link></li>
-              <li><Link onClick={() => { setShow("") }} to="/videos" >Videos</Link></li>
-              <li><Link onClick={() => { setShow("") }} to="/contact" >Contact</Link></li>
-            </ul>
-          </div>
-          <div className="social">
-            <ul>
-              <li>
-                <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square fa-2x"></i></a>
-              </li>
-              <li>
-                <a href="https://www.youtube.com/channel/UC9u2sGj3VZpR0KAGCF_BvUw" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube-square fa-2x"></i></a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/yogialunette/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square fa-2x"></i></a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
-              </li>
-            </ul>
-          </div>
+        <div className="hamburger">
+          <div></div>
         </div>
       </div>
+
+      {show &&
+        <div id="sidebar">
+          <div className="links">
+            <div className="side-list">
+              <ul>
+                <li><Link onClick={() => { setShow(!show); document.getElementById("togglerCheckbox").checked = false; }} to="/about" >About</Link></li>
+                <li><Link onClick={() => { setShow(!show); document.getElementById("togglerCheckbox").checked = false; }} to="/videos" >Videos</Link></li>
+                <li><Link onClick={() => { setShow(!show); document.getElementById("togglerCheckbox").checked = false; }} to="/contact" >Contact</Link></li>
+              </ul>
+            </div>
+            <div className="social">
+              <ul>
+                <li>
+                  <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer"><i className="fab fa-facebook-square fa-2x"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.youtube.com/channel/UC9u2sGj3VZpR0KAGCF_BvUw" target="_blank" rel="noopener noreferrer"><i className="fab fa-youtube-square fa-2x"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.instagram.com/yogialunette/" target="_blank" rel="noopener noreferrer"><i className="fab fa-instagram-square fa-2x"></i></a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin fa-2x"></i></a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      }
     </Fragment>
   )
 }
+
 
 export default Sidebar;
 
