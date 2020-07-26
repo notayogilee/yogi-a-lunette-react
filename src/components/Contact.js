0017fc7886bad1b81194982372d03aabbd2776b5
Lee Castelani <notayogilee@gmail.com>
2020-07-26 10:35:43 -0400

    work on contact transition and some refactor of repeat code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,6 +6,9 @@ const Contact = () => {
   const [transition, setTransition] = useState(false);
   const [id, setId] = useState(0);
 
+  const undoTransition = function () {
+    setId(0); setTransition(false);
+  }
 
   return (
     <div className="contact-page">
@@ -19,29 +22,42 @@ const Contact = () => {
           </a>
           <a href="#" onClick={(() => { setId(2); setTransition(!transition) })}>
             <div className="bubble-2">
-              <i class="far fa-comments fa-4x"></i>
+              <i class="far fa-envelope fa-4x"></i>
               <h1>contact</h1>
 
             </div>
           </a>
-          <a href="#" onClick={(() => { setId(3); setTransition(!transition) })}>
+          {/* <a href="#" onClick={(() => { setId(3); setTransition(!transition) })}>
             <div className="bubble-3">
               <i class="far fa-envelope fa-4x"></i>
               <h1>message</h1>
             </div>
-          </a>
+          </a> */}
           {/* {transition && id === 1 &&} */}
 
           {transition && id === 2 &&
+            // Added double click because of google complete action - can change to single click
+            <div className="contact" onClick={undoTransition}>
+              <div id="contact-form">
+                <h2>yogialunette@gmail.com</h2>
 
-            <div className="contact" onClick={() => { setTransition(""); setId(0) }}>
-              <h2>claudiaviens@gmail.com</h2>
-              <a href="https://www.facebook.com/pages/category/Yoga-Studio/Yogi-a-Lunette-252704625430721/" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-facebook-square fa-2x"></i>
-              </a>
-              <a href="https://www.linkedin.com/in/claudia-viens-8233b1aa/" target="_blank" rel="noopener noreferrer">
-                <i className="fab fa-linkedin fa-2x"></i>
-              </a>
+                <form name="contact" method="post" data-netlify="true">
+                  <input type="hidden" name="form-name" value="contact" />
+                  <div onClick="(e) => e.preventDefault()" className="text-fields">
+                    <input type="text" name="name" className="text-input name-input" placeholder="Name" />
+                  </div>
+                  <div onClick="(e) => e.preventDefault()" className="text-fields">
+                    <input type="email" name="email" className="text-input email-input" placeholder="Your email address" />
+                  </div>
+                  <div onClick="(e) => e.preventDefault()" className="text-fields">
+                    <input type="text" name="subject" className="text-input subject-input" placeholder="Subject" />
+                  </div>
+                  <div onClick="(e) => e.preventDefault()" className="text-fields">
+                    <textarea name="message" className="text-input message-input" placeholder="Enter Message"></textarea>
+                  </div>
+                  <button className="message-button" type="submit">SUBMIT</button>
+                </form>
+              </div>
             </div>
           }
 
@@ -49,7 +65,7 @@ const Contact = () => {
 
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
