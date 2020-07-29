cf3572aa40e065b7c0ebcaf142b681a0243f0e03
Lee Castelani <notayogilee@gmail.com>
2020-07-29 19:26:06 -0400

    start to re-style video page

--- src/components/VideoNew.js
+++ src/components/VideoNew.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 import videos from '../_data/video.json';
 import ReactPlayer from 'react-player';
 import Pagenation from './Pagenation';
@@ -9,7 +9,7 @@ const VideoNew = () => {
 
   // const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [videosPerPage, setVideosPerPage] = useState(6);
+  const [videosPerPage, setVideosPerPage] = useState(2);
 
   const indexOfLastVideo = currentPage * videosPerPage;
   const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
@@ -21,31 +21,46 @@ const VideoNew = () => {
   const totalPages = Math.ceil(videos.length / videosPerPage);
 
   return (
-    <div className="video-container">
-      <ul>
-        {currentVideos.map(video => (
-          <li key={video.id}>
-            <h3>{video.title}</h3>
-            <p>{video.description}</p>
-            <ReactPlayer
-              className="clip"
-              controls
-              width="320px"
-              height="180px"
-              url={video.url}
-            />
-          </li>
-        ))}
-      </ul>
-      {currentPage > 1 &&
-        <a href="#" onClick={() => setCurrentPage(currentPage - 1)}>PREV</a>
-      }
+    <Fragment>
 
-      <Pagenation videosPerPage={videosPerPage} totalVideos={videos.length} paginate={paginate} />
-      {currentPage < totalPages &&
-        <a href="#" onClick={() => setCurrentPage(currentPage + 1)}>NEXT</a>
-      }
-    </div>
+      <div className="video-container">
+        <ul>
+          {currentVideos.map(video => (
+            <div className="video">
+              <li key={video.id}>
+                <h3>{video.title}</h3>
+                <div className="clip">
+                  <ReactPlayer
+                    controls
+                    width="320px"
+                    height="180px"
+                    url={video.url}
+                  />
+                </div>
+                <div className="paragraph">{video.description}</div>
+              </li>
+            </div>
+          ))}
+        </ul>
+      </div>
+
+      <footer className="pagenation-footer">
+        {currentPage > 1 &&
+
+          <a href="#" onClick={() => setCurrentPage(currentPage - 1)}>PREV</a>
+
+        }
+
+        <Pagenation videosPerPage={videosPerPage} totalVideos={videos.length} paginate={paginate} />
+
+
+        {currentPage < totalPages &&
+
+          <a href="#" onClick={() => setCurrentPage(currentPage + 1)}>NEXT</a>
+
+        }
+      </footer>
+    </Fragment>
   )
 };
 
