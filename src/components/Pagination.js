d9c74cd71ffa7b1d381ceb635b16a68d7ba33260
Lee Castelani <notayogilee@gmail.com>
2020-07-30 16:57:29 -0400

    video page is lined up

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import './Pagenation.scss';
+import React, { useState } from 'react';
+import './Pagination.scss';
 
-const Pagenation = ({ videosPerPage, totalVideos, paginate }) => {
 
+const Pagenation = ({ videosPerPage, totalVideos, paginate, currentPage }) => {
+
+  const [highlight, setHighlight] = useState(currentPage);
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalVideos / videosPerPage); i++) {
     pageNumbers.push(i);
   }
-
   return (
     <nav>
       <ul className="list">
         {pageNumbers.map(number => (
           <li key={number} className="page-number">
-            <a onClick={() => paginate(number)} href="#" className="page-link">
+            <a onClick={() => { paginate(number); setHighlight(true) }} href="#" id="page-link" className={highlight}>
               {number}
             </a>
           </li>
--- src/components/VideoNew.js
+++ src/components/VideoNew.js
@@ -1,7 +1,7 @@
 import React, { useState, Fragment } from 'react';
 import videos from '../_data/video.json';
 import ReactPlayer from 'react-player';
-import Pagenation from './Pagenation';
+import Pagination from './Pagination';
 import './VideoNew.scss';
 
 const VideoNew = () => {
@@ -9,7 +9,7 @@ const VideoNew = () => {
 
   // const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-  const [videosPerPage, setVideosPerPage] = useState(2);
+  const [videosPerPage, setVideosPerPage] = useState(4);
 
   const indexOfLastVideo = currentPage * videosPerPage;
   const indexOfFirstVideo = indexOfLastVideo - videosPerPage;
@@ -24,42 +24,42 @@ const VideoNew = () => {
     <Fragment>
 
       <div className="video-container">
-        <ul>
+        <ul className="video-list">
           {currentVideos.map(video => (
-            <div className="video">
-              <li key={video.id}>
-                <h3>{video.title}</h3>
-                <div className="clip">
-                  <ReactPlayer
-                    controls
-                    width="320px"
-                    height="180px"
-                    url={video.url}
-                  />
-                </div>
-                <div className="paragraph">{video.description}</div>
-              </li>
-            </div>
+
+            <li key={video.id} >
+              <h3>{video.title}</h3>
+
+              <ReactPlayer
+                controls
+                width="90%"
+                height="180px"
+                url={video.url}
+              />
+
+              <div className="paragraph">{video.description}</div>
+            </li>
+
           ))}
         </ul>
       </div>
 
-      <footer className="pagenation-footer">
-        {currentPage > 1 &&
+      <div className="pagination-footer">
+        <div className="prev">
+          {currentPage > 1 &&
+            <a href="#" onClick={() => setCurrentPage(currentPage - 1)}>PREV</a>
+          }
+        </div>
 
-          <a href="#" onClick={() => setCurrentPage(currentPage - 1)}>PREV</a>
-
-        }
-
-        <Pagenation videosPerPage={videosPerPage} totalVideos={videos.length} paginate={paginate} />
-
-
-        {currentPage < totalPages &&
-
-          <a href="#" onClick={() => setCurrentPage(currentPage + 1)}>NEXT</a>
-
-        }
-      </footer>
+        <div className="pagination">
+          <Pagination videosPerPage={videosPerPage} totalVideos={videos.length} paginate={paginate} currentPage={currentPage} />
+        </div>
+        <div className="next">
+          {currentPage < totalPages &&
+            <a href="#" onClick={() => setCurrentPage(currentPage + 1)}>NEXT</a>
+          }
+        </div>
+      </div>
     </Fragment>
   )
 };
