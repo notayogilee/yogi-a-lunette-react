e9062ba1666ab56bb3f2f2dae8616aa0afab7510
Lee Castelani <notayogilee@gmail.com>
2020-08-11 15:46:01 -0400

    add some font styles for about page

--- src/components/About.js
+++ src/components/About.js
@@ -12,7 +12,7 @@ const About = () => {
         <br />
         <p>Claudia s'inspire de nombreux maîtres enseignants descendants du Hatha Yoga, comme: Iyengar, Anusara, Ashtanga, Bikram et de la technologie Kundalini. Elle est professeure certifiée en Hatha-Vinyasa du Collège Sangha Yoga. Elle est aussi vloggeuse sur sa chaîne Youtube "Yogi-à-lunette". Créatrice de classes qui sortent de l'ordinaire, de tutoriels regorgeant de détails et d'ateliers surprenants dans l'intégration de  différentes pratiques,  Claudia vous fera vivre une expérience hors du commun à chaque rencontre. Depuis 2019,  elle s'est joint à la grande famille IDOLEM qui lui a ouvert sa porte et son cœur, permettant de transmettre, à cette communauté désireuse de co-créer, son amour du Yoga.</p>
         <br />
-        <p>Claudia  réside dans le quartier St-Henri de Montréal avec son mari, sa sœur et  deux toutous gentils comme tout, Loky & Jasper. Ces grandes passions sont le yoga, la lecture, la cuisine et les aventures dans les friperies et les ventes de garage !</p>
+        <p>Claudia  réside dans le quartier St-Henri de Montréal avec son mari, sa sœur et  deux toutous gentils comme tout, Loky & Jasper. Ces grandes passions sont le yoga, la lecture, la cuisine et les aventures dans les friperies et les ventes de garage!</p>
       </div>
     </section>
   )
