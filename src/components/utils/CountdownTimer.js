02974cd80659d5859d2f781d4c4fcdb060e73faf
Lee Castelani <notayogilee@gmail.com>
2020-11-16 17:02:20 -0500

    change date

--- src/components/utils/CountdownTimer.js
+++ src/components/utils/CountdownTimer.js
@@ -12,7 +12,7 @@ export default function Timer() {
   const [showTimer, setShowTimer] = useState(true);
 
   const startTimer = () => {
-    const countdownDate = new Date('November 16, 2020 18:00:00').getTime();
+    const countdownDate = new Date('November 23, 2020 18:00:00').getTime();
 
     interval = setInterval(() => {
       const now = new Date().getTime();
